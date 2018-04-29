1f1bb5a720e15709c9f23c089b9d78897b4f2f00
prosvet <prosvet@prosvet.ru>
2018-04-29 14:44:01 +0300

    pro-5 models book and author

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    surname = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    patronymic = models.CharField(max_length=200)
+    birth_date = models.DateTimeField('date birthday')
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    read_date = models.DateTimeField('date published')
+    authors = models.ManyToManyField(Author)
