abe5c6ed73edb1359ecddc80502255273c8c42e8
prosvet <prosvet@prosvet.ru>
2018-05-05 14:35:27 +0300

    pro-7 Add horizontal-filter for authors

--- books/admin.py
+++ books/admin.py
@@ -6,6 +6,12 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('surname', 'name', 'patronymic')
 
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'pub_date', 'read_date')
+    list_filter = ('pub_date',)
+    filter_horizontal = ('authors',)
+
+
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(User)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
