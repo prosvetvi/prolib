d2fbbfe60d3ace403e987f34b6d38951bcbf8d93
prosvet <prosvet@prosvet.ru>
2018-05-07 07:50:55 +0300

    pro-10 Styles, template, add page to views

--- books/views.py
+++ books/views.py
@@ -1,11 +1,17 @@
 from books.models import Book
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     latest_books_list = Book.objects.order_by('-pub_date')[:5]
     output = ', '.join([b.name for b in latest_books_list])
-    return HttpResponse(output)
+    return render(
+        request,
+        'index.html',
+        context={'num_books': output, 'num_instances': output, 'num_instances_available': output,
+                 'num_authors': output, 'num_visits': output},
+    )
 
 
 def detail(request, book_id):
